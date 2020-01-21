81da5dc658fcaf2f8bf56087d40099203def2f18
CregskiN <272473132@qq.com>
2020-01-21 18:01:15 +0800

    axios is more useful than ajax and fetch

--- src/pages/Order/store/reducer.js
+++ src/pages/Order/store/reducer.js
@@ -8,15 +8,27 @@ const defaultState = fromJS({
         description: '好吃不贵',
         imgUrl: 'http://hxsmallgame.cn/caidanimg/luroufan.jpg',
         amount: 12,
+        food_id: 0,
         current_price: 15,
         original_price: 20,
     }],// 点餐菜单
     totalPrice: 123, // 已选商品总价
+    foodTypeId: 0, // 当前展示菜品种类编号
+    foodTypeList: [], // 餐品种类列表
 
+    isMenuSelectorVisible: false, // 菜单选择器是否可见
 
 
 
 });
+
+/*
+* 需要的action
+* 1. 控制menu选择器显隐
+* 2. 切换当前显示的typeId
+* 3.
+* */
+
 
 export default (state = defaultState, action) => {
     switch (action.type) {
